0c81cb267ec91e11564a41c121db32ffbe10d7be
Qbxs <ip4ssi@gmail.com>
2021-09-17 13:12:58 +0200

    add precedence to render

--- src/Prop.hs
+++ src/Prop.hs
@@ -7,17 +7,20 @@ data Expr
   | Conj Expr Expr
   | Disj Expr Expr
   | Impl Expr Expr
- deriving (Show, Eq, Ord)
+ deriving (Eq, Ord)
+
+instance Show Expr where
+  showsPrec _  (Atom p)   = showChar p
+  showsPrec pr (Neg p)    = showParen (pr > 3) $ showChar '¬' . showsPrec 3 p
+  showsPrec pr (Conj p q) = showParen (pr > 2) $ showsPrec 3 p . showChar '∧' . showsPrec 2 q
+  showsPrec pr (Disj p q) = showParen (pr > 1) $ showsPrec 2 p . showChar '∨' . showsPrec 1 q
+  showsPrec pr (Impl p q) = showParen (pr > 0) $ showsPrec 1 p . showChar '→' . shows q
 
 class Render a where
   render :: a -> String
 
 instance Render Expr where
-  render (Atom c) = pure c
-  render (Neg p) = '¬':render p
-  render (Conj p q) = "(" <> render p <> "∧" <> render q <> ")"
-  render (Disj p q) = "(" <> render p <> "∨" <> render q <> ")"
-  render (Impl p q) = "(" <> render p <> "→" <> render q <> ")"
+  render = show
 
 isAtom :: Expr -> Bool
 isAtom (Atom _) = True
