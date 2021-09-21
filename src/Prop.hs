d0bab1797f85224cae11dd21eaa3f9c087c76711
Qbxs <ip4ssi@gmail.com>
2021-09-21 11:49:53 +0200

    add exprs for predicate logic

--- src/Prop.hs
+++ src/Prop.hs
@@ -7,14 +7,18 @@ data Expr
   | Conj Expr Expr
   | Disj Expr Expr
   | Impl Expr Expr
+  | Forall Char Expr
+  | Exists Char Expr
  deriving (Eq, Ord)
 
 instance Show Expr where
-  showsPrec _  (Atom p)   = showChar p
-  showsPrec pr (Neg p)    = showParen (pr > 3) $ showChar '¬' . showsPrec 3 p
-  showsPrec pr (Conj p q) = showParen (pr > 2) $ showsPrec 3 p . showChar '∧' . showsPrec 2 q
-  showsPrec pr (Disj p q) = showParen (pr > 1) $ showsPrec 2 p . showChar '∨' . showsPrec 1 q
-  showsPrec pr (Impl p q) = showParen (pr > 0) $ showsPrec 1 p . showChar '→' . shows q
+  showsPrec _  (Atom x)   = showChar x
+  showsPrec pr (Neg p)    = showParen (pr > 5) $ showChar '¬' . showsPrec 5 p
+  showsPrec pr (Conj p q) = showParen (pr > 4) $ showsPrec 5 p . showChar '∧' . showsPrec 4 q
+  showsPrec pr (Disj p q) = showParen (pr > 3) $ showsPrec 4 p . showChar '∨' . showsPrec 3 q
+  showsPrec pr (Impl p q) = showParen (pr > 2) $ showsPrec 3 p . showChar '→' . showsPrec 2 q
+  showsPrec pr (Forall x p) = showChar '∀' . showChar x . showChar '.' . showParen (pr > 1) (showsPrec 1 p)
+  showsPrec pr (Exists x p) = showChar '∃' . showChar x . showChar '.' . showParen (pr > 0) (shows p)
 
 class Render a where
   render :: a -> String
